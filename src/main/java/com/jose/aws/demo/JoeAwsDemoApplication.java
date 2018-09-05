1c31f8f0b37c090debdc4172c21e0caadd778e26
Joseph Madhanu <jmadhanu@nvidia.com>
2018-09-05 16:59:57 -0700

    creating demo project, first commit

---
+++ src/main/java/com/jose/aws/demo/JoeAwsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.jose.aws.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoeAwsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoeAwsDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jose/aws/demo/JoeAwsDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jose.aws.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoeAwsDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
